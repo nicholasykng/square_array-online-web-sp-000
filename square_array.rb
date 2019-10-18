1f7f640bc4374ba3b59999065504ed43776df060
Nicholas Ng <nicholasykng@gmail.com>
2019-10-18 03:36:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  new_numbers = []
-  new_numbers = array.each{|i| i ** 2}
-  return new_numbers
+  new_array = []
+  array.each do |integer|
+  new_array << integer ** 2
 end
+return new_array
 end
